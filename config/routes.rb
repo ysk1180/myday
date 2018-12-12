32e1c069ca2b7cfddf3b57162a8bfb6d0a869901
Yusuke Miyazaki <ysk1180@gmail.com>
2018-12-12 21:13:05 +0900

    [add]フッター追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'terms', to: 'terms#index', as: :terms
+  get 'privacy', to: 'privacies#index', as: :privacy
   resources :posts, only: [:create]
-  root to: "posts#new"
+  root to: 'posts#new'
   post 'make', to: 'posts#make'
 end
